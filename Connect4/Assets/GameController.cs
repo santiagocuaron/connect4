3a5ee7f49d63a8291936e6b13e2557078db52799
santiagocuaron <santiagocuaron@github.com>
2021-03-03 19:24:13 -0600

    Changing Chip Sprite

---
+++ Connect4/Assets/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Sprite RedChip;
+    public Sprite BlueChip;
+    GameObject Chip_A1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chip_A1 = GameObject.Find("Chips/Chip_A1");
+        Chip_A1.GetComponent<SpriteRenderer>().sprite = RedChip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
